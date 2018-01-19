00fbc74bf260a6720f1ea875632f1fcf76fdb7df
Arver1 <aactepanov@mail.ru>
2018-01-19 18:34:41 +0300

    amended js for links style

--- js/script.js
+++ js/script.js
@@ -1,74 +1,65 @@
 'use strict';
 (function() {
+
 	var mainMenu = document.querySelector('.main_menu');
+	var mainMenuLinks= mainMenu.querySelectorAll('a');
 	var logo = document.querySelector('.main_nav > a');
 	var userBlock = document.querySelector('.user_block');
+	var userBlockLinks = userBlock.querySelectorAll('a');
 	var slide1 = document.querySelector('.slide_1');
 	var slide2 = document.querySelector('.slide_2');
 	var slide3 = document.querySelector('.slide_2');
 	var popUp = document.querySelector('.popup_write_us');
 	var linkPopUp = document.querySelector('.contacts a');
-	var close = document.querySelector('.popup_write_us .close');
-	if (mainMenu != null) {
-		var mainMenuLinks= mainMenu.querySelectorAll('a');
-		mainMenu.addEventListener ('click', function (evt) {
-			for(var i = 0; i < mainMenuLinks.length; i++) {
-				if (mainMenuLinks[i].classList.contains('active_link')) {
-					mainMenuLinks[i].classList.remove('active_link');
-					break;
-				}
-			}
-			if (localStorage.getItem('userBlock')) {
-				userBlockLinks[localStorage.getItem('userBlock')].classList.remove('active_link');
-				localStorage.removeItem('userBlock');
-			}
-			if (evt.target.tagName.toLowerCase() === 'a') {
-				evt.target.classList.add('active_link');
-				for(i = 0; i < mainMenuLinks.length; i++) {
-					if (mainMenuLinks[i].classList.contains('active_link')) {
-						localStorage.setItem('mainMenu', i);
-						break;
-					}
-				}
-			}
-		});
-	}
-	if (userBlock != null) {
-		var userBlockLinks = userBlock.querySelectorAll('a');
-		userBlock.addEventListener ('click', function (evt) {
-			for(var i = 0; i < userBlockLinks.length; i++) {
-				if (userBlockLinks[i].classList.contains('active_link')) {
-					userBlockLinks[i].classList.remove('active_link');
-					break;
-				}
-			}
-			if (localStorage.getItem('mainMenu')) {
-				mainMenuLinks[localStorage.getItem('mainMenu')].classList.remove('active_link');
-				localStorage.removeItem('mainMenu');
-			}
-			if (evt.target.tagName.toLowerCase() === 'a') {
-				evt.target.classList.add('active_link');
-				for(i = 0; i < userBlockLinks.length; i++) {
-					if (userBlockLinks[i].classList.contains('active_link')) {
-						localStorage.setItem('userBlock', i);
-						break;
-					}
-				}
-			}
-		});
-	}
+	var close = popUp.querySelector('.close');
+
+	var arrLinks = [];
+	if (mainMenuLinks) arrLinks = arrLinks.concat(Array.prototype.slice.call(mainMenuLinks));
+	if (userBlockLinks) arrLinks = arrLinks.concat(Array.prototype.slice.call(userBlockLinks));
+
 	logo.addEventListener('click', function () {
 		localStorage.removeItem('mainMenu');
 		localStorage.removeItem('userBlock');
 	});
+
 	document.addEventListener('DOMContentLoaded', function () {
 		if (localStorage.getItem('userBlock')) {
-			mainMenuLinks[localStorage.getItem('userBlock')].classList.add('active_link');
+			arrLinks[localStorage.getItem('userBlock')].classList.add('active_link');
 		}
 		if (localStorage.getItem('mainMenu')) {
-			mainMenuLinks[localStorage.getItem('mainMenu')].classList.add('active_link');
+			arrLinks[localStorage.getItem('mainMenu')].classList.add('active_link');
 		}
 	});
+
+	if (mainMenuLinks) {
+		mainMenu.addEventListener ('click', function (evt) {
+			if(evt.target.tagName.toLowerCase() === 'a') {
+				changeArrLinks('active_link', evt.target, 'mainMenu');
+			}
+		});
+	}
+	
+	if (userBlockLinks) {
+		userBlock.addEventListener ('click', function (evt) {
+			if(evt.target.tagName.toLowerCase() === 'a') {
+				changeArrLinks('active_link', evt.target, 'userBlock');
+			}
+		});
+	}
+
+	function changeArrLinks(className, currentLink, localStorageName, array = arrLinks) {
+		for(var i = 0; i < arrLinks.length; i++) {
+			if (arrLinks[i].classList.contains(className)) {
+				arrLinks[i].classList.remove(className);
+				break;
+			}
+		}
+		currentLink.classList.add('active_link');
+		if (currentLink.getAttribute('href') != '#') localStorage.setItem(localStorageName, arrLinks.indexOf(currentLink));
+	}
+
+	// ----- PopUp ----- 
+
 	linkPopUp.addEventListener('click', function(evt) {
 		evt.preventDefault();
 		popUp.style.display = 'flex';
@@ -86,4 +77,6 @@
 			popUp.style.display = 'none';
 		}
 	});
+	
+	// -----end PopUp ----- 
 })();
